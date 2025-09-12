62488118d0d22eeebd4b3ed01b0cb6bc4a6632a9
GuilleDLP <guillermodelaparra.sm@gmail.com>
2025-09-12 10:09:06 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,13 @@
 
-const CACHE_NAME = "reporte-visitas-cache-v1";
+const CACHE_NAME = "reporte-visitas-cache-v4";
 const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json",
+  "./auth.js",
+  "./admin-panel.js",
+  "./firebase-sync.js",
+  "./firebase-users.js",
   "./icono-192.png",
   "./icono-512.png"
 ];
